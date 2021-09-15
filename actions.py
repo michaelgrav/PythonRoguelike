d619e9cef584796a226c2c1bf783bd8b947e2a29
Michael Graves <mdgrav@protonmail.com>
2021-09-15 14:29:09 -0500

    Reworked the action class and check if tile can be moved on

--- actions.py
+++ actions.py
@@ -1,16 +1,49 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        """Perform this action with the objects needed to determine its scope
+
+        `engine` is the scope of this action is being performed in
+
+        `entity` is the object performing the action
+
+        This method must be overridden by Action subclasses
+        """
+
+        raise NotImplementedError()
 
 
 # Subclass of action
+# Raise SystemExit
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
 
 
 # Subclass of action
+# Check if the move is inbounds and on a walkable tile, if neither is true we move the entity
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return  # Destination is out of bounds
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return  # Destination is blocked by a tile
+
+        entity.move(self.dx, self.dy)
--- engine.py
+++ engine.py
@@ -3,7 +3,6 @@ from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
 
-from actions import EscapeAction, MovementAction
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -29,12 +28,7 @@ class Engine:
             if action is None:
                 continue
 
-            if isinstance(action, MovementAction):
-                if self.game_map.tiles["walkable"][self.player.x + action.dx, self.player.y + action.dy]:
-                    self.player.move(dx=action.dx, dy=action.dy)
-
-            elif isinstance(action, EscapeAction):
-                raise SystemExit()
+            action.perform(self, self.player)
 
     # Handles drawing to the screen, iterate though self.entities and print them
     def render(self, console: Console, context: Context):
