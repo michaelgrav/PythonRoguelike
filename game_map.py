8eb04148b82c3d668129e9b06d95db94dfa40b24
Michael Graves <mdgrav@protonmail.com>
2021-09-18 12:44:57 -0500

    Updated tiles to have the "light" attribute

--- game_map.py
+++ game_map.py
@@ -1,4 +1,4 @@
-import numpy as np # type: ignore
+import numpy as np  # type: ignore
 from tcod.console import Console
 
 import tile_types
@@ -11,10 +11,16 @@ class GameMap:
         # Create a 2d array, filled with the same values. It Basically fills self.tiles with floor tiles
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        # Tiles the player can currently see
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        # Tiles the player has seen before
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
     # Makes sure the player doesn't leave the map
     def in_bounds(self, x: int, y: int) -> bool:
         """Return true if x and y are inside of the bounds of this map"""
         return 0 <= x < self.width and 0 <= y < self.height
 
+    # START HERE
     def render(self, console: Console) -> None:
         console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
