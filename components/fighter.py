0212868bb48f0e832e67f0da1ddd780004155537
Michael Graves <mdgrav@protonmail.com>
2021-09-22 14:36:16 -0500

    Enemies now die, updated the hp method to properly set hp

--- components/fighter.py
+++ components/fighter.py
@@ -1,7 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from components.base_component import BaseComponent
+
+if TYPE_CHECKING:
+    from entity import Actor
 
 
 class Fighter(BaseComponent):
+    entity: Actor
+
     def __init__(self, hp: int, defense: int, power: int):
         self.max_hp = hp  # Hit Points
         self._hp = hp
@@ -14,4 +23,30 @@ class Fighter(BaseComponent):
 
     @hp.setter
     def hp(self, value: int) -> None:
-        self._hp = max(0, max(value, self.max_hp))  # Hp will never be less than 0, and can't go higher than max
+        self._hp = max(0, min(value, self.max_hp))  # Hp will never be less than 0, and can't go higher than max
+        if self._hp == 0 and self.entity.ai:  # if hp is 0, kill the entity
+            self.die()
+
+    def die(self) -> None:
+        # Print out a message indicating the death of the enemy
+        if self.engine.player is self.entity:
+            death_message = "You died!"
+        else:
+            death_message = f"{self.entity.name} is dead!"
+
+        # Set the entity’s character to “%” (most roguelikes use this for corpses)
+        self.entity.char = "%"
+
+        # Set its color to red
+        self.entity.color = (191, 0, 0)
+
+        # Set blocks_movement to False, so that the entities can walk over the corpse
+        self.entity.blocks_movement = False
+
+        # Remove the AI from the entity, so it’ll be marked as dead and won’t take any more turns
+        self.entity.ai = None
+
+        # Change the name to this
+        self.entity.name = f"remains of {self.entity.name}"
+
+        print(death_message)
